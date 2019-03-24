22a106bd8a40348dfbb6447787fbb9ff0559852a
Isuru Fernando <isuruf@gmail.com>
2019-03-23 22:27:11 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,20 @@
 
 mkdir build_yaafe
 cd build_yaafe
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DWITH_FFTW3=ON -DWITH_HDF5=ON -DWITH_LAPACK=ON -DWITH_MPG123=ON -DWITH_EIGEN_LIBRARY=ON ..
-make
+
+if [[ "$c_compiler" == "gcc" ]]; then
+  export PATH="${PATH}:${BUILD_PREFIX}/${HOST}/sysroot/usr/lib"
+fi
+
+cmake \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DCMAKE_PREFIX_PATH=$PREFIX \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DWITH_FFTW3=ON -DWITH_HDF5=ON \
+  -DWITH_LAPACK=ON \
+  -DWITH_MPG123=ON \
+  -DWITH_EIGEN_LIBRARY=ON \
+  ..
+
+make -j${CPU_COUNT}
 make install
